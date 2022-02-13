b0cd63e74847ddaa228990b18b0203b21e6e00cd
moohammadrashidy <mouhamadrashidy@gmail.com>
2022-02-13 17:37:57 +0330

    spaceInvaders react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+//import './App.css';
+//import ScriptTag from 'react-script-tag';
+import Main from './Main'
+
+
+
+function App() {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
